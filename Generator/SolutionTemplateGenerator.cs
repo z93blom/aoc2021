47cdbddbc28b767544a180e98a31d967b45e1bd0
Fredrik Blom <fredrik@familjenblom.se>
2021-12-01 06:31:57 +0100

    Updated template file

--- Generator/SolutionTemplateGenerator.cs
+++ Generator/SolutionTemplateGenerator.cs
@@ -5,32 +5,35 @@ namespace AdventOfCode.Generator
 
     public class SolutionTemplateGenerator {
         public static string Generate(Problem problem) {
-            return $@"namespace AdventOfCode.Y{problem.Year}.Day{problem.Day:00};
-                 |
-                 |class Solution : ISolver
-                 |{{
-                 |    public int Year => {problem.Year};
-                 |
-                 |    public int Day => {problem.Day};
-                 |
-                 |    public string GetName() => ""{problem.Title}"";
-                 |
-                 |    public IEnumerable<object> Solve(string input)
-                 |    {{
-                 |        yield return PartOne(input);
-                 |        yield return PartTwo(input);
-                 |     }}
-                 |
-                 |    object PartOne(string input)
-                 |    {{
-                 |        return 0;
-                 |    }}
-                 |
-                 |    object PartTwo(string input)
-                 |    {{
-                 |        return 0;
-                 |    }}
-                 |}}".StripMargin();
+            return
+              $@"using AdventOfCode.Utilities;
+                |
+                |namespace AdventOfCode.Y{problem.Year}.Day{problem.Day:00};
+                |
+                |class Solution : ISolver
+                |{{
+                |    public int Year => {problem.Year};
+                |
+                |    public int Day => {problem.Day};
+                |
+                |    public string GetName() => ""{problem.Title}"";
+                |
+                |    public IEnumerable<object> Solve(string input)
+                |    {{
+                |        yield return PartOne(input);
+                |        yield return PartTwo(input);
+                |     }}
+                |
+                |    object PartOne(string input)
+                |    {{
+                |        return 0;
+                |    }}
+                |
+                |    object PartTwo(string input)
+                |    {{
+                |        return 0;
+                |    }}
+                |}}".StripMargin();
         }
     }
 }
